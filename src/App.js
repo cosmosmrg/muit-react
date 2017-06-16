4e3cf798beed367bec6f799ab1ebf7c7c0ee5d79
May <calopc.canova@gmail.com>
2017-06-16 12:38:49 +0700

    with material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import PropTypes from 'prop-types'
+import axios from 'axios'
+import { BrowserRouter, Route, Link }  from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class CrazyButton extends Component{
   constructor(props){
@@ -10,23 +15,67 @@ class CrazyButton extends Component{
     this.props.onClick()
   }
   render() {
+    const style = {
+      margin: 12,
+    };
     return (
       <div>
-        <button onClick={()=> this.onClick()}>{this.props.title}</button>
+        <RaisedButton label={this.props.title} primary={true} style={style} onClick={()=> this.onClick()}/>
+        {/* <button onClick={()=> this.onClick()}>{this.props.title}</button> */}
         <p>{this.props.nClick}</p>
       </div>
     );
   }
 
 }
-class App extends Component {
+CrazyButton.propTypes ={
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  nClick: PropTypes.number.isRequired
+}
+class CurrencyConverter extends Component{
+  render(){
+    const amount = this.props.amount
+    const rate = this.props.rate
+    if(rate === null){
+      return(<div>
+        <p>Loading</p>
+      </div>)
+    }
+    else{
+      return(
+        <div>
+          <TextField
+            id="text-field-input"
+            type='text'
+            defaultValue={amount}
+            onChange={(e) => this.props.onAmountChange(e.target.value)}
+          />
+          {/* <input
+            type='text'
+            value={amount}
+            onChange={(e) => this.props.onAmountChange(e.target.value)}
+          /> */}
+          <p>THB: {amount*rate.THB}</p>
+          <p>EURO: {amount*rate.EUR}</p>
+        </div>
+      )
+    }
+
+  }
+}
+CurrencyConverter.propTypes ={
+  rate: PropTypes.object.isRequired,
+  amount: PropTypes.number.isRequired,
+  onAmountChange: PropTypes.func.isRequired
+}
+class SuperClick extends Component{
   constructor(props) {
     super(props)
     this.state = {
       clicks: [0,0,0]
     }
   }
-
   onChildClick(nChild){
     this.setState((st)=> {
       const newClick = [... st.clicks]
@@ -34,23 +83,74 @@ class App extends Component {
       return{clicks:newClick}
     })
   }
+  render() {
+    return (
+      <div>
+      {
+        [0,1,2].map((x) => (
+          <CrazyButton key={'button'+x} nClick={this.state.clicks[x]} title={'button'+(x+1)} onClick={() => this.onChildClick(x)}/>
+        ))
+      }
+      <p>total : {this.state.clicks.reduce((a, b) => a + b, 0)}</p>
+      </div>
+    )
+  }
+
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ratesData: null,
+      amount: 0
+    }
+  }
+  componentDidMount(){
+    this.loadRates()
+  }
+  loadRates(){
+    axios.get('http://api.fixer.io/latest?base=USD')
+    .then((response) => {
+      console.log(response);
+      this.setState({ratesData: response.data.rates})
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+  onAmountChange(amount){
+    this.setState({amount:amount})
+  }
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        {
-          [0,1,2].map((x) => (
-            <CrazyButton key={'button'+x} nClick={this.state.clicks[x]} title={'button'+(x+1)} onClick={() => this.onChildClick(x)}/>
-          ))
-        }
-        <p>total : {this.state.clicks.reduce((a, b) => a + b, 0)}</p>
+        <div>
+            <div style={{}}>
+              <NavLink to="/converter">converter</NavLink>
+              <NavLink to="/superclick">superclick</NavLink>
+            </div>
+            <Route path='/converter' render={() =>
+              (
+                <CurrencyConverter
+                  rate={this.state.ratesData}
+                  amount={this.state.amount}
+                  onAmountChange = {(newAmount)=>this.onAmountChange(newAmount)}
+                />
+              )}/>
+
+
+            <Route path='/superclick' render={() =>(
+              <SuperClick/>
+            )}/>
+        </div>
+
       </div>
+      </BrowserRouter>
     );
   }
 }
-CrazyButton.propTypes ={
-  title: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-  nClick: PropTypes.number.isRequired
-}
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const A = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(
-  <App />,
+  <A />,
   document.getElementById('root')
 );
